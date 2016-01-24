c451c7c9af208cc2a24db1d740107577af542882
RunEMC <runlin.li@gmail.com>
2016-01-24 04:58:02 -0500

    Updated video

--- js/scrip.js
+++ js/scrip.js
@@ -1,42 +1,41 @@
 
 $(document).ready(function() {
     $(window).stellar();
-    
+
 });
 
 $(document).ready(
 
-  function() { 
+    function() { 
 
-    $("html").niceScroll({
-        cursorcolor:"rgba(30,30,30,.5)",
-        zindex:999,
-        scrollspeed:80,
-        mousescrollstep:50,
-        cursorborder:"0px solid #fff",
-    });
-      
+        $("html").niceScroll({
+            cursorcolor:"rgba(30,30,30,.5)",
+            zindex:999,
+            scrollspeed:80,
+            mousescrollstep:50,
+            cursorborder:"0px solid #fff",
+        });
 
-  }
+
+    }
 
 );
 
-
 $(function() {
-	  $('a[href*=#]:not([href=#])').click(function() {
-	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 
-	      var target = $(this.hash);
-	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-	      if (target.length) {
-	        $('html,body').animate({
-	          scrollTop: target.offset().top
-	        }, 1000);
-	        return false;
-	      }
-	    }
-	  });
-	});
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
 
 
 
